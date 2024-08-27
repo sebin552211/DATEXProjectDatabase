b283457806cfa26b37e3892c0d92c96ef119f75c
A-liya-s-alim <aliyasalim2001@gmail.com>
2024-08-27 12:20:32 +0530

    signal r

---
+++ DATEX_ProjectDatabase/SignalR/MailStatusHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using MySqlX.XDevAPI;
+
+namespace DATEX_ProjectDatabase.SignalR
+{
+    public class MailStatusHub: Hub
+    {
+        public async Task NotifyMailStatusUpdated()
+        {
+            await Clients.All.SendAsync("MailStatusUpdated");
+        }
+    }
+}
